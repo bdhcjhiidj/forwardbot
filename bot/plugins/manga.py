11f0c96fbc96839ae8d7032c50b2f6776afbbd6d
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-01-31 14:09:54 +0530

    Create manga.py

---
+++ bot/plugins/manga.py
@@ -0,0 +1,23 @@
+from . import *
+
+INLOCK = "`Seems like inline messages aren't allowed here`"
+
+
+@bot.on(events.NewMessage(incoming=True,pattern="/manga")
+async def manga(e):
+    msg = await e.reply("`Searching ...`")
+    keyword = e.text.split(" ", maxsplit=1)[1]
+    if keyword is None:
+        return await msg.edit("`Provide a Keyword to search`")
+    try:
+        animes = await user.inline_query("animedb_bot", f"<m> {keyword}")
+        ok = await animes[0].click(
+            e.chat_id,
+            reply_to=e.reply_to_msg_id,
+            silent=True if e.is_reply else False,
+            hide_via=True,
+        )
+        await bot.send_message(e.chat_id, ok)
+        return await msg.delete()
+    except Exception:
+        return await msg.edit("`No Results Found`")
