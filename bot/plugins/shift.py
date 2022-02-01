782dfa6fe7c5c0dd606cee3bc1274941a2fe0cf6
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-02-01 19:05:57 +0530

    Create shift.py

---
+++ bot/plugins/shift.py
@@ -0,0 +1,33 @@
+from . import *
+
+
+
+@bot.on(events.NewMessage(pattern="shift (.*)"))
+async def _(e):
+    x = e.pattern_match.group(1)
+    z = await e.reply("`processing..`")
+    a, b = x.split("|")
+    try:
+        c = int(a)
+    except Exception:
+        try:
+            c = (await bot.get_entity(a)).id
+        except Exception:
+            await z.edit("invalid Channel given")
+            return
+    try:
+        d = int(b)
+    except Exception:
+        try:
+            d = (await bot.get_entity(b)).id
+        except Exception:
+            await z.edit("invalid Channel given")
+            return
+    async for msg in user.iter_messages(int(c), reverse=True):
+        try:
+            await asyncio.sleep(0.5)
+            m = await bot.get_messages(int(c), ids=msg.id)
+            await bot.send_message(int(d), m)
+        except:
+            pass
+    await z.edit("Done")
