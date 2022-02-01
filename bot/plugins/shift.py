ac084ff3bfade19fbfe656da24585062b829725b
kaif-00z <88398455+kaif-00z@users.noreply.github.com>
2022-02-01 16:43:40 +0000

    pyLint: auto-fixes

--- bot/plugins/shift.py
+++ bot/plugins/shift.py
@@ -4,7 +4,7 @@ from . import *
 @bot.on(events.NewMessage(incoming=True, pattern="/shift (.*)"))
 async def _(e):
     if not (is_auth(e.sender_id)):
-        return 
+        return
     x = e.pattern_match.group(1)
     z = await e.reply("`processing..`")
     a, b = x.split("|")
